a06dc3eb9673e7e3bd149468d9d3bec2cb5b6157
Benjamin Hoody <benjamin.hoody@mullen.pvt.k12.co.us>
2022-05-23 13:12:07 -0600

    first commit

---
+++ Basketball Player Maker/src/BasketballPlayer.java
@@ -0,0 +1,22 @@
+
+public class BasketballPlayer
+	{
+		String name;
+		int hight;
+		String position;
+		int shootingAbility;
+		int handleAbility;
+		
+
+		
+		public BasketballPlayer(String n, int h, String p, int s, int ha)
+		{
+			name = n;
+			hight = h;
+			position = p;
+			shootingAbility = s;
+			handleAbility = ha;
+		}
+		
+		
+	}
---
+++ Basketball Player Maker/src/BasketballPlayerMaker.java
@@ -0,0 +1,11 @@
+
+public class BasketballPlayerMaker
+	{
+
+		public static void main(String[] args)
+			{
+				
+
+			}
+
+	}
